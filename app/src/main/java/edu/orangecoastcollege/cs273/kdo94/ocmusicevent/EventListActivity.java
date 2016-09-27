a2eec12ffe970855a39704eaa19a7ce3bc6559f7
kdo94 <kdo94@student.cccd.edu>
2016-09-27 15:38:42 -0700

    Initial commit

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/kdo94/ocmusicevent/EventListActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.kdo94.ocmusicevent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EventListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event_list);
+    }
+}
