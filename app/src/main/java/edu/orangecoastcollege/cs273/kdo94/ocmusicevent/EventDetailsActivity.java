7639d4549043344f674f631f763c8a2c0f6fab92
kdo94 <kdo94@student.cccd.edu>
2016-09-29 14:41:07 -0700

    OC Music Event version 1

--- app/src/main/java/edu/orangecoastcollege/cs273/kdo94/ocmusicevent/EventDetailsActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kdo94/ocmusicevent/EventDetailsActivity.java
@@ -1,13 +1,56 @@
 package edu.orangecoastcollege.cs273.kdo94.ocmusicevent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class EventDetailsActivity extends AppCompatActivity {
+
+    public ImageView eventImageView;
+    public TextView eventTitleTextView;
+    public TextView eventDetailsTextView;
+
+    private String title;
+    private String details;
+    private String imageFileName;
+
+    private Context context = (Context) this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_details);
+
+        eventImageView = (ImageView) findViewById(R.id.eventImageView);
+        eventTitleTextView = (TextView) findViewById(R.id.eventTitleTextView);
+        eventDetailsTextView = (TextView) findViewById(R.id.eventDetailsTextView);
+
+        Intent intentFromList = getIntent();
+        title = intentFromList.getStringExtra("Title");
+        details = intentFromList.getStringExtra("Details");
+
+        eventTitleTextView.setText(title);
+        eventDetailsTextView.setText(details);
+        imageFileName = title.replace(" ", "" ) + ".jpeg";
+
+        AssetManager am = context.getAssets();
+
+        try{
+           InputStream stream = am.open(imageFileName);
+            Drawable image = Drawable.createFromStream(stream, title);
+            eventImageView.setImageDrawable(image);
+        }
+        catch(IOException ex){
+            Log.e("OC Music Events", "Cannot load image" + imageFileName + ex);
+        }
     }
 }
--- app/src/main/java/edu/orangecoastcollege/cs273/kdo94/ocmusicevent/EventListActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/kdo94/ocmusicevent/EventListActivity.java
@@ -1,13 +1,31 @@
 package edu.orangecoastcollege.cs273.kdo94.ocmusicevent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class EventListActivity extends AppCompatActivity {
+public class EventListActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_list);
+
+        setListAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, MusicEvent.titles ));
+
+        // setContentView(R.layout.activity_event_list);
+    }
+
+    protected void onListItemClick(ListView l, View v, int pos, long id){
+        String title = MusicEvent.titles[pos];
+        String details = MusicEvent.details[pos];
+
+        Intent detailsIntent = new Intent(this, EventDetailsActivity.class);
+        detailsIntent.putExtra("Title", title);
+        detailsIntent.putExtra("Details", details);
+        startActivity(detailsIntent);
+
     }
 }
